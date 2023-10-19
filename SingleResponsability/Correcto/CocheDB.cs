ef215202fd185838a98e253ea7bce21c39c6d716
JMSPere <perejoanms@gmail.com>
2023-10-19 12:38:35 +0200

    Added Single Responsability Principle implementation Closes #1

---
+++ SingleResponsability/Correcto/CocheDB.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrincipiosSolid.SingleResponsability.Correcto
+{
+    /*
+     *Decoupled version: DB access
+     */
+    public class CocheDB
+    {
+        void guardarCocheDB(Coche coche)
+        {
+
+        }
+    }
+}
