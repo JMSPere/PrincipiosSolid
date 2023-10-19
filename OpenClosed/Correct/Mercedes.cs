12ee96abbbc4c96484192c975031b6c0b530edc3
JMSPere <perejoanms@gmail.com>
2023-10-19 14:16:21 +0200

    open-close principle example

---
+++ OpenClosed/Correct/Mercedes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrincipiosSolid.OpenClosed.Correct
+{
+    internal class Mercedes : Coche
+    {
+        public override int PrecioMedioCoche() { return 27000; }
+    }
+}
---
+++ OpenClosed/Incorrect/Coche.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrincipiosSolid.OpenClosed.Incorrect
+{
+    public class Coche
+    {
+        String marca;
+
+        public Coche(string marca) { this.marca = marca; }   
+
+        public String GetMarcaCoche() { return marca; }
+    }
+}
