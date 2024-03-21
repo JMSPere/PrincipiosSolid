f80421906fb7cad3304e4a56a7a5ba6608ad19de
Pere Joan <perejoanms@gmail.com>
2024-03-21 13:45:33 +0100

    updated Liskov Substitution

--- LiskovSubstitution/Correct/MoreShapes.cs
+++ LiskovSubstitution/Correct/MoreShapes.cs
@@ -6,7 +6,35 @@ using System.Threading.Tasks;
 
 namespace PrincipiosSolid.LiskovSubstitution.Correct
 {
-    internal class MoreShapes
+    /*
+     * In the following example we use the IShape interface, which defines a method called GetSides(). 
+     * Both classes Triangle and Rectangle implement this interface and have its own implementation fo the GetSides() method.
+     */
+    public interface IShape
     {
+        string GetSides();
     }
+
+    public class Rectangle : IShape
+    {
+        public string GetSides()
+        {
+            return "4";
+        }
+    }
+
+    public class Triangle : IShape
+    {
+        public string GetSides()
+        {
+            return "3";
+        }
+    }
+
+    /*
+     * IShape shape = new Triangle();
+     * Console.WriteLine(shape.GetSides()); Shows "3"
+     * shape = new Rectangle();
+     * Console.WriteLine(shape.GetSides()); Shows "4"
+     */
 }
--- LiskovSubstitution/Incorrect/MoreShapes.cs
+++ LiskovSubstitution/Incorrect/MoreShapes.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PrincipiosSolid.LiskovSubstitution.Incorrect;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,29 @@ using System.Threading.Tasks;
 
 namespace PrincipiosSolid.LiskovSubstitution.Incorrect
 {
-    internal class MoreShapes
+    /*
+     * This example does not implement Liskov Substitution principle because the derived class Triangle changes the GetSides() 
+     * method functionality. That's due to the fact that in this example we are assuming we can use the instantiation of Triangle the same way we could 
+     * use an instantiation of Rectangle and this is not true in this scenario.
+     */
+    public class Rectangle
     {
+        public virtual string GetSides()
+        {
+            return "4";
+        }
     }
+
+    public class Triangle : Rectangle
+    {
+        public override string GetSides()
+        {
+            return "3";
+        }
+    }
+
+    /*
+     * Rectangle rectangle = new Triangle();
+     * Console.WriteLine(Rectangle.GetSides()); Shows "3" when it should show "4"
+     */
 }
