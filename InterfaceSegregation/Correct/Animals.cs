c7061e4854093ea26ac636e5c88a9e148bd305db
Pere Joan <perejoanms@gmail.com>
2024-03-21 14:01:24 +0100

    added Interface Segregation

---
+++ InterfaceSegregation/Correct/Animals.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrincipiosSolid.InterfaceSegregation.Correct
+{
+    /*
+     * In this example we can se how specific the given interfaces are, giving flexibility and reusability to the code while
+     * only having to implement the necessary methods for each class.
+     */
+    public interface IEat
+    {
+        void Eat();
+    }
+
+    public interface ISleep
+    {
+        void Sleep();
+    }
+
+    public interface ISwim
+    {
+        void Swim();
+    }
+
+    public interface IFly
+    {
+        void Fly();
+    }
+
+    public class Fish : IEat, ISleep, ISwim
+    {
+        public void Eat() { /*...*/ }
+        public void Sleep() {/*...*/}
+        public void Swim() { /*...*/ }
+    }
+
+    public class Bird : IEat, ISleep, IFly
+    {
+        public void Eat() { /*...*/ }
+        public void Sleep() { /*...*/ }
+        public void Fly() { /*...*/ }
+    }
+}
---
+++ InterfaceSegregation/Incorrect/Animals.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrincipiosSolid.InterfaceSegregation.Incorrect
+{
+    /*
+     * In this code we can see how the Animals interface has four methods. In certain scenarios this means it has to many methods, which will cause the classes 
+     * implementing this interface will be forced to implement each method even if the class doesn't need it or shouldn't do it, resulting in useless methods for 
+     * some classes.
+     */
+    public interface IAnimals
+    {
+        void Eat();
+        void Sleep();
+        void Swim();
+        void Fly();
+    }
+
+    public class Fish : IAnimals
+    {
+        public void Eat()
+        {
+            // Code for Eat
+        }
+
+        public void Fly()
+        {
+            throw new NotImplementedException(); // A fish can't fly
+        }
+
+        public void Sleep()
+        {
+            // Code for sleep
+        }
+
+        public void Swim()
+        {
+            // Code for swim
+        }
+    }
+}
